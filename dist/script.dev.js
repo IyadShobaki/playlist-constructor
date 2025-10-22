9331b3e93ec572469f8512234074000cf0df9855
Iyad <iyadshobaki82@gmail.com>
2025-10-22 19:35:11 -0400

    Add elements using template instead of create element method

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -3,6 +3,7 @@
 var container = document.querySelector(".container");
 var songsContainer = container.querySelector(".songs-container");
 var noSongsElement = container.querySelector(".no-songs");
+var songTemplate = document.querySelector("#song-template").content.querySelector(".song");
 var addSongForm = container.querySelector("#add-song-form");
 var title = document.querySelector("#song-title-input");
 var artist = document.querySelector("#song-artist-input");
@@ -12,31 +13,13 @@ function renderHasSongs() {
 }
 
 function addSong(artistValue, titleValue) {
-  // Create an <li> and give it the "song" class
-  var songElement = document.createElement("li");
-  songElement.classList.add("song"); // Create song elements
-
-  var artistElement = document.createElement("h3");
-  artistElement.classList.add("song__artist");
+  // Using song template
+  var songElement = songTemplate.cloneNode(true);
+  var artistElement = songElement.querySelector(".song__artist");
   artistElement.textContent = artistValue;
-  var titleElement = document.createElement("p");
-  titleElement.classList.add("song__title");
+  var titleElement = songElement.querySelector(".song__title");
   titleElement.textContent = titleValue;
-  var likeButtonElement = document.createElement("button");
-  likeButtonElement.classList.add("song__button", "song__button_type_like");
-  likeButtonElement.setAttribute("type", "button"); // Append the artist, title, and button elements to the song element
-
-  songElement.append(artistElement, titleElement, likeButtonElement);
   songsContainer.append(songElement);
-  /* songsContainer.innerHTML += `
-    <li class="song">
-      <h3 class="song__artist">${artistValue}</h3>
-      <p class="song__title">${titleValue}</p>
-      <button type="button" class="song__button song__button_type_like"></button>
-    </li>
-  `; */
-  // Return the song element
-
   return songElement;
 }
 
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container");
 const songsContainer = container.querySelector(".songs-container");
 const noSongsElement = container.querySelector(".no-songs");
+const songTemplate = document
+  .querySelector("#song-template")
+  .content.querySelector(".song");
 
 const addSongForm = container.querySelector("#add-song-form");
 const title = document.querySelector("#song-title-input");
@@ -11,36 +14,16 @@ function renderHasSongs() {
 }
 
 function addSong(artistValue, titleValue) {
-  // Create an <li> and give it the "song" class
-  const songElement = document.createElement("li");
-  songElement.classList.add("song");
+  // Using song template
+  const songElement = songTemplate.cloneNode(true);
 
-  // Create song elements
-  const artistElement = document.createElement("h3");
-  artistElement.classList.add("song__artist");
+  const artistElement = songElement.querySelector(".song__artist");
   artistElement.textContent = artistValue;
 
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("song__title");
+  const titleElement = songElement.querySelector(".song__title");
   titleElement.textContent = titleValue;
 
-  const likeButtonElement = document.createElement("button");
-  likeButtonElement.classList.add("song__button", "song__button_type_like");
-  likeButtonElement.setAttribute("type", "button");
-
-  // Append the artist, title, and button elements to the song element
-  songElement.append(artistElement, titleElement, likeButtonElement);
-
   songsContainer.append(songElement);
-  /* songsContainer.innerHTML += `
-    <li class="song">
-      <h3 class="song__artist">${artistValue}</h3>
-      <p class="song__title">${titleValue}</p>
-      <button type="button" class="song__button song__button_type_like"></button>
-    </li>
-  `; */
-
-  // Return the song element
   return songElement;
 }
 
